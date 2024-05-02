77a6cc5084b5f7343defe4f8103883e9bce9c23f
Devin Atkin <devinatkin@gmail.com>
2024-05-02 16:24:55 -0600

    Lab 4.2 Started

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -2,5 +2,8 @@ set layout [readnet spice $project.lvs.spice]
 set source [readnet spice /dev/null]
 readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $source
 readnet verilog ../src/project.v $source
-readnet spice ../xschem/simulation/challenge.spice $source
+readnet spice ../xschem/spice_files/top.spice $source
+readnet spice ../xschem/spice_files/oscillator_20MHZ.spice
+readnet spice ../xschem/spice_files/oscillator_21MHZ.spice
+readnet spice ../xschem/spice_files/inverter.spice
 lvs "$layout $project" "$source $project" $::env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
